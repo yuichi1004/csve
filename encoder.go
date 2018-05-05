90124f320632ff74c352e03f482bf9edf4b49567
Yuichi Murata <yuichi1004@gmail.com>
2018-05-05 09:53:23 +0900

    CustomEncoder implemented

--- encoder.go
+++ encoder.go
@@ -9,12 +9,20 @@ import (
 	"github.com/pkg/errors"
 )
 
+// CustomerEncoder inject your custom encode process.
+// Return true as ok if this logic handles the encode, otherwise Encode() will
+// fallback to default encode process.
+type CustomEncoder func(e *Encoder, v reflect.Value, format string) (ok bool, raw string, err error)
+
 // Encoder writes values into csv writer.
 type Encoder struct {
 	*csv.Writer
 
 	// Spcify location to be used encoding. If not specified, Encoder use time.UTC.
 	Location *time.Location
+
+	// Custom encoder to custiomize encoding process.
+	CustomEncoder CustomEncoder
 }
 
 // NewEncoder returns a new Encoder which encodes values into csv writer.
@@ -22,7 +30,7 @@ type Encoder struct {
 // NOTE: useHeder is not implemented yet.
 func NewEncoder(writer *csv.Writer, useHeader bool) (*Encoder, error) {
 	return &Encoder{
-		writer, time.UTC,
+		writer, time.UTC, nil,
 	}, nil
 }
 
@@ -53,10 +61,20 @@ func (e *Encoder) Encode(v interface{}) (err error) {
 	encoded := make([]string, len(fields))
 	for i, f := range fields {
 		ref := rv.FieldByIndex(f.fieldindex)
+
+		var ok bool
 		var err error
-		encoded[i], err = f.enc(e, ref, f.csvformat)
-		if err != nil {
-			return errors.Errorf("field %s encode failed", f.fieldname)
+		if e.CustomEncoder != nil {
+			ok, encoded[i], err = e.CustomEncoder(e, ref, f.csvformat)
+			if err != nil {
+				return errors.Errorf("field %s encode failed", f.fieldname)
+			}
+		}
+		if !ok {
+			encoded[i], err = f.enc(e, ref, f.csvformat)
+			if err != nil {
+				return errors.Errorf("field %s encode failed", f.fieldname)
+			}
 		}
 	}
 
--- encoder_test.go
+++ encoder_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"reflect"
 	"testing"
 	"time"
 )
@@ -17,7 +18,8 @@ func TestEncoder_Encode(t *testing.T) {
 	loc, _ := time.LoadLocation("Asia/Tokyo")
 
 	type fields struct {
-		Location *time.Location
+		Location      *time.Location
+		CustomEncoder CustomEncoder
 	}
 	type args struct {
 		v interface{}
@@ -50,6 +52,25 @@ func TestEncoder_Encode(t *testing.T) {
 			want: "str,-1,-32,-64,32,64,3.2,6.4,2017-12-24T15:30:00\n",
 		},
 		{
+			name: "normal case with custom encoder",
+			fields: fields{
+				CustomEncoder: func(e *Encoder, v reflect.Value, format string) (ok bool, raw string, err error) {
+					val := v.Interface()
+					var t time.Time
+					if t, ok = val.(time.Time); ok {
+						if t.IsZero() {
+							return true, "N/A", nil
+						}
+					}
+					return false, "", nil
+				},
+			},
+			args: args{
+				&TestData{"str", -1, -32, -64, 32, 64, 3.2, 6.4, time.Time{}},
+			},
+			want: "str,-1,-32,-64,32,64,3.2,6.4,N/A\n",
+		},
+		{
 			name: "pointer case",
 			fields: fields{
 				Location: time.UTC,
@@ -74,8 +95,9 @@ func TestEncoder_Encode(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			buf := new(bytes.Buffer)
 			e := &Encoder{
-				Writer:   csv.NewWriter(buf),
-				Location: tt.fields.Location,
+				Writer:        csv.NewWriter(buf),
+				Location:      tt.fields.Location,
+				CustomEncoder: tt.fields.CustomEncoder,
 			}
 			if err := e.Encode(tt.args.v); (err != nil) != tt.wantErr {
 				t.Errorf("Encoder.Encode() error = %v, wantErr %v", err, tt.wantErr)
@@ -111,4 +133,39 @@ func ExampleEncoder_Encode() {
 	encoder.Flush()
 
 	// Output: 5,Yuichi,2017-12-24T15:30:00
+}
+
+func ExampleCustomEncoder() {
+	v := struct {
+		ID      int       `csv:"0,id"`
+		Name    string    `csv:"1,name"`
+		Created time.Time `csv:"2,created,2006-01-02T15:04:05"`
+	}{
+		5, "Yuichi", time.Time{},
+	}
+
+	csvWriter := csv.NewWriter(os.Stdout)
+	encoder, err := NewEncoder(csvWriter, false)
+	if err != nil {
+		fmt.Printf("failed to create encoder: %v\n", err)
+		return
+	}
+	encoder.CustomEncoder = func(e *Encoder, v reflect.Value, format string) (ok bool, raw string, err error) {
+		val := v.Interface()
+		var t time.Time
+		if t, ok = val.(time.Time); ok {
+			if t.IsZero() {
+				return true, "N/A", nil
+			}
+		}
+		return false, "", nil
+	}
+
+	if err := encoder.Encode(&v); err != nil {
+		fmt.Printf("failed to parse csv: %v\n", err)
+		return
+	}
+	encoder.Flush()
+
+	// Output: 5,Yuichi,N/A
 }
